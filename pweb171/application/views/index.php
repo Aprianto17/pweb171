55fbce7a7439cad818d7c699e2720641f94233ec
Aprianto17 <32377173+Aprianto17@users.noreply.github.com>
2017-12-26 18:05:53 +0800

    PWEB171

---
+++ pweb171/application/views/index.php
@@ -0,0 +1,37 @@
+
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Lagu Anak</title>
+    <link rel="stylesheet" href="/css/style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <h1 id="judul">Koleksi lagu anak</h1>
+    <hr>
+<div class="">
+
+    <div id="menu">
+
+      <a href="<?php echo site_url('welcome/menu1') ?>">Pelangi</a><br>
+      <a href="<?php echo site_url('welcome/menu2') ?>">Balon Ku</a><br>
+      <a href="<?php echo site_url('welcome/menu3') ?>">Kereta</a><br>
+      <a href="<?php echo site_url('welcome/menu4') ?>">Cicak</a><br>
+      <a href="<?php echo site_url('welcome/menu5') ?>">Kebun Ku</a><br>
+
+    </div>
+
+<div id="isi">
+  test
+</div>
+</div>
+    <br>
+    <br>
+    didesain oleh: Aprianto; 2017
+
+
+
+
+
+  </body>
+</html>
