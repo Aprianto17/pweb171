6b2b8fc942b7be1a341074260f917c799bf0d485
Aprianto17 <32377173+Aprianto17@users.noreply.github.com>
2018-02-21 20:19:54 +0800

    blog171(04)
    comit 04
    profil user

---
+++ blog171/application/views/profil.php
@@ -0,0 +1,19 @@
+<div class="sign-up-form">
+  <h3 class="tittle reg">My Provil <i class="glyphicon glyphicon-user"></i></h3>
+    <div class="col-md-2">
+      <h4 class="a">Nama : </h4>
+    </div>
+     <h4 class="tittle"><?= $userid['nama'] ?></h4>
+     <br>
+     <div class="col-md-2">
+       <h4 class="a">Email : </h4>
+     </div>
+     <h4 class="tittle"><?=$userid['email']?></h4>
+     <br>
+     <br>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
